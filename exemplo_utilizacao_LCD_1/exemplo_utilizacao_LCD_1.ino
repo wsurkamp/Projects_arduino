52725afb530a93fa073356699d8385c0dc56e2ad
Christian W. Surkamp <christianwkamp1@gmail.com>
2015-11-06 16:42:38 -0200

    exemplos utilização display LCD

---
+++ exemplo_utilizacao_LCD_1/exemplo_utilizacao_LCD_1.ino
@@ -0,0 +1,15 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12,11,5,4,3,2);
+
+void setup(){
+  lcd.begin(16,2);
+  lcd.setCursor(0,0);
+  lcd.print("Ola, meu nome e:");
+  lcd.setCursor(0,1);
+  lcd.print("Christian");
+}
+
+void loop(){
+  
+}
