610f30b168b3bf2d6d19c63e0d76e9d3cccb7663
Christian W. Surkamp <christianwkamp1@gmail.com>
2015-11-10 04:11:23 -0200

    otimizando exemplo conexao banco

--- exemplo_conex_o_mysql_connector/exemplo_conex_o_mysql_connector.ino
+++ exemplo_conex_o_mysql_connector/exemplo_conex_o_mysql_connector.ino
@@ -19,23 +19,56 @@ Connector my_conn; // The Connector/Arduino reference
 char user[] = "root";
 char password[] = "root";
 char INSERT_SQL[] = 
- "INSERT INTO test_arduino.hello VALUES ('Hello, MySQL!', NULL)";
+ "INSERT INTO test_arduino.hello VALUES ('Hello, MySQL2!', NULL)";
 
 void setup() {
   Ethernet.begin(mac_addr);
   Serial.begin(115200);
   delay(1000);
-  Serial.println("Connecting...");
-  if (my_conn.mysql_connect(server_addr, 3306, user, password))
-  {
-    delay(500);
-     /* Write Hello, World to MySQL table test_arduino.hello */
-     my_conn.cmd_query(INSERT_SQL);
-     Serial.println("Query Success!"); 
-  } 
-  else
-    Serial.println("Connection failed.");
+  if (my_conn.is_connected()) {
+    Serial.println("Conection Active!");
+    Serial.println("Insert into DB...");
+    my_conn.cmd_query(INSERT_SQL);
+    Serial.println("Success!");
+  } else {
+    my_conn.disconnect();
+    Serial.println("Reconnecting...");
+    if (my_conn.mysql_connect(server_addr, 3306, user, password)) {
+      delay(500);
+      my_conn.cmd_query(INSERT_SQL);
+      Serial.println("Success!");
+    } else
+      Serial.println("Connect failed!");
+  }
 }
 
 void loop() {
+  delay(1000);
+  if (Serial.available() > 0){
+    if (my_conn.is_connected()) {
+      Serial.println("Insert into DB...");
+      Serial.println(Serial.read());
+      my_conn.cmd_query(INSERT_SQL);
+    } else {
+      my_conn.disconnect();
+      Serial.println("Connecting...");
+      if (my_conn.mysql_connect(server_addr, 3306, user, password)) {
+        delay(500);
+        Serial.println("Success!");
+      } else {
+        Serial.println("Connect failed!");
+      }
+    }
+      
+      
+      
+      /*
+    Serial.println("Connecting new...");
+    Serial.println(Serial.read());
+    
+      delay(500);
+      /* Write Hello, World to MySQL table test_arduino.hello 
+      my_conn.cmd_query(INSERT_SQL);
+      Serial.println("Query Success!!!!!!!!!!!!"); */
+  }
 }
