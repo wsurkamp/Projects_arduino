26f4f7c658b513f62a62a36f450cb2f955b1b447
Christian W. Surkamp <christianwkamp1@gmail.com>
2015-12-01 05:53:23 -0200

    ajustes exemplos conexão banco de dados

--- exemplo_conex_o_mysql_connector/exemplo_conex_o_mysql_connector.ino
+++ exemplo_conex_o_mysql_connector/exemplo_conex_o_mysql_connector.ino
@@ -1,45 +1,32 @@
-/**
-* Example: Hello, MySQL!
-*
-* This code module demonstrates how to create a simple 
-* database-enabled sketch.
-*/
 #include "SPI.h"
 #include "Ethernet.h"
 #include "sha1.h"
 #include "mysql.h"
 
-/* Setup for Ethernet Library */
 byte mac_addr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress server_addr(192, 168, 1, 7);
+IPAddress ip(192,168,1,3);
+IPAddress server_addr(192, 168, 1, 2);
 
-/* Setup for the Connector/Arduino */
-Connector my_conn; // The Connector/Arduino reference
+Connector my_conn;
 
 char user[] = "root";
-char password[] = "root";
-char INSERT_SQL[] = 
- "INSERT INTO test_arduino.hello VALUES ('Hello, MySQL2!', NULL)";
+char password[] = "surkamp";
+char INSERT_SQL[] = "INSERT INTO test_arduino.hello VALUES ('Hello, MySQL2!', NULL)";
+char INSERT_TEMP_SQL[] = "INSERT INTO test_arduino.hello VALUES ('Temperatura obtida no sensor - %d ', NULL)";
 
 void setup() {
-  Ethernet.begin(mac_addr);
+  Ethernet.begin(mac_addr, ip);
   Serial.begin(115200);
-  delay(1000);
-  if (my_conn.is_connected()) {
-    Serial.println("Conection Active!");
-    Serial.println("Insert into DB...");
+  Serial.print("Arduino IP ");
+  Serial.println(Ethernet.localIP());
+  Serial.println("Conecting...");
+  
+  if (my_conn.mysql_connect(server_addr, 3306, user, password)) {
+    delay(500);
     my_conn.cmd_query(INSERT_SQL);
-    Serial.println("Success!");
-  } else {
-    my_conn.disconnect();
-    Serial.println("Reconnecting...");
-    if (my_conn.mysql_connect(server_addr, 3306, user, password)) {
-      delay(500);
-      my_conn.cmd_query(INSERT_SQL);
-      Serial.println("Success!");
-    } else
-      Serial.println("Connect failed!");
-  }
+    Serial.println("Query Success!");
+  } else
+    Serial.println("Connect failed!");
 }
 
 void loop() {
@@ -47,8 +34,16 @@ void loop() {
   if (Serial.available() > 0){
     if (my_conn.is_connected()) {
       Serial.println("Insert into DB...");
-      Serial.println(Serial.read());
-      my_conn.cmd_query(INSERT_SQL);
+      int letter = Serial.read();
+      char query[100];
+      char letterChar[10];
+      dtostrf(letter, 1, 1, letterChar);
+      
+      sprintf(query, INSERT_TEMP_SQL, letter);
+      
+      Serial.println(letter);
+      Serial.println(query);
+      my_conn.cmd_query(query);
     } else {
       my_conn.disconnect();
       Serial.println("Connecting...");
@@ -59,16 +54,5 @@ void loop() {
         Serial.println("Connect failed!");
       }
     }
-      
-      
-      
-      /*
-    Serial.println("Connecting new...");
-    Serial.println(Serial.read());
-    
-      delay(500);
-      /* Write Hello, World to MySQL table test_arduino.hello 
-      my_conn.cmd_query(INSERT_SQL);
-      Serial.println("Query Success!!!!!!!!!!!!"); */
   }
 }
--- exemplo_gravar_banco_leitura_serial/exemplo_gravar_banco_leitura_serial.ino
+++ exemplo_gravar_banco_leitura_serial/exemplo_gravar_banco_leitura_serial.ino
@@ -1,13 +1,15 @@
-/**
-* Example: Hello, MySQL!
-*
-* This code module demonstrates how to create a simple 
-* database-enabled sketch.
-*/
 #include "SPI.h"
 #include "Ethernet.h"
 #include "sha1.h"
 #include "mysql.h"
+#include <LiquidCrystal.h>
+
+const int LM35 = 2;
+float temperatura = 0;
+int ADClido = 0;
+int cont = 10;
+
+LiquidCrystal lcd(12,11,5,4,3,2);
 
 /* Setup for Ethernet Library */
 byte mac_addr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
@@ -20,8 +22,17 @@ char user[] = "root";
 char password[] = "root";
 char INSERT_SQL[] = 
  "INSERT INTO test_arduino.hello VALUES (%s, NULL)";
+char INSERT_TEMP_SQL[] = 
+ "INSERT INTO test_arduino.temperatura VALUES (Temperatura obtida no sensor = %s *C, NULL)";
 
 void setup() {
+  lcd.begin(16,2);
+  lcd.setCursor(0,0);
+  lcd.print("Ola Christian W.");
+  lcd.setCursor(0,1);
+  lcd.print("Surkamp");
+  delay(10000);
+  
   Ethernet.begin(mac_addr);
   Serial.begin(9600);
   delay(1000);
@@ -42,41 +53,28 @@ void setup() {
 }
 
 void loop() {
-  if (Serial.available() > 0){
-    String novaMensagem = leStringSerial();
+  ADClido = analogRead(LM35);
+  temperatura = ADClido * 0.48828125;
+  
+  lcd.setCursor(0,0);
+  lcd.print("Temperatura =   ");
+  lcd.setCursor(0,1);
+  lcd.print(temperatura);
+  lcd.print(" *C");
+  delay(500);
+  
+  cont++;
+  
+  if (cont > 10){
 
     char query[100];
+    char temperature[10];
+    dtostrf(temperatura, 1, 1, temperature);
     /*preparando query*/
-    sprintf(query, INSERT_SQL, novaMensagem);
+    sprintf(query, INSERT_TEMP_SQL, temperature);
     
     Serial.print("Novo comando de inserço SQL = ");
     Serial.println(query);
   }
 }
 
-/**
- * Função que lê uma string da Serial
- * e retorna-a
- */
-String leStringSerial(){
-  String conteudo = "";
-  char caractere;
-  
-  // Enquanto receber algo pela serial
-  while(Serial.available() > 0) {
-    // Lê byte da serial
-    caractere = Serial.read();
-    // Ignora caractere de quebra de linha
-    if (caractere != '\n'){
-      // Concatena valores
-      conteudo.concat(caractere);
-    }
-    // Aguarda buffer serial ler próximo caractere
-    delay(10);
-  }
-    
-  Serial.print("Recebi: ");
-  Serial.println(conteudo);
-    
-  return conteudo;
-}
